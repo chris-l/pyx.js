1e9c5cdc32d745210ade52c9ba01ebc21a553545
Christopher Luna <dev@christopher-luna.com>
2013-08-24 14:21:36 -0500

    Added gruntfile and travis

---
+++ Gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg     : grunt.file.readJSON('package.json'),
+        jshint  : {
+            all     : ['Gruntfile.js', 'lib/pyx.js']
+        },
+        'jasmine-node' : {
+            options : {
+                coffee  : false,
+                noStack : false
+            },
+            run     : {
+                spec    : "test/"
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-jasmine-node');
+
+    // Default task(s).
+    grunt.registerTask('default', [
+        'jshint',
+    ]);
+
+};
+
